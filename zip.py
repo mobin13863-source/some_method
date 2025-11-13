bfdd18497a5e4e8002403199ee72ffea88d36ede
Mobin <234991811+mobin13863-source@users.noreply.github.com>
2025-11-13 18:27:03 +0330

    zip
    this a  zip you can see in code have that work

---
+++ zip.py
@@ -0,0 +1,12 @@
+
+name=["mobin", "jadi", "sina"]
+
+family=["hojati", "mir", "san"]
+
+for i in zip(name, family):
+    print(i )
+
+print("new_line")
+
+for j in zip("mobin", "sina"):
+    print(j)
